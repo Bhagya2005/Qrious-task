61a08af4fe7d14ee60c6e50d02d0be287eeee738
Bhagya N. Patel <114048548+Bhagya2005@users.noreply.github.com>
2025-10-01 17:18:03 +0530

    Update task10.js
    add logic

--- day01/task10.js
+++ day01/task10.js
@@ -1,8 +1,9 @@
-
+//Here I sort value of price in ascending order 
 function task10(drinks) {
     return drinks.sort((a, b) => a.price - b.price);
 }
 
 
 console.log(task10([{name:"lemonade", price:50},{name:"lime", price:10}]));
+
 

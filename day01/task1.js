e0bfc0864dc1b48a82870c38a0b848205d00fd15
Bhagya N. Patel <114048548+Bhagya2005@users.noreply.github.com>
2025-10-01 17:16:04 +0530

    Update task1.js
    add logic and test cases

--- day01/task1.js
+++ day01/task1.js
@@ -1,3 +1,9 @@
+//in this senario  initially take variable i the apply for loop 
+//and check i th element j th element 
+//if it is not match then i++
+//if thi senario if we initially i=0 then our ans is i+1
+//if some senario we take value of i = 1 then our ans is i
+
 function task01(nums) {
     let i = 0;
     for (let j = 1; j < nums.length; j++) {
@@ -11,4 +17,5 @@ function task01(nums) {
 
 
 console.log(task01([1, 1, 2]));
+
 
